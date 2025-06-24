3844e71fc8c39aa7b636e5df20330ca8d1ae72bb
celinem13 <celinemangahas13@gmail.com>
2025-06-24 02:03:16 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { ThemeProvider } from './context/ThemeContext'; // ⬅️ Import provider
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider> {/* ⬅️ Wrap entire app */}
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
