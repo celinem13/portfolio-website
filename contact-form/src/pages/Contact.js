773f67a23d8a69dca8746250482f7c76d9df3e74
celinem13 <celinemangahas13@gmail.com>
2025-06-24 00:05:10 -0700

    turning everything into react
    turning it all into react now

---
+++ contact-form/src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section>
+      <h1>Contact Me</h1>
+      <p>This is the contact form page.</p>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ contact-form/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <section>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This will be your homepage content.</p>
+    </section>
+  );
+}
+
+export default Home;
